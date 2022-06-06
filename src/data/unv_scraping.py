3d86cc696345c58baa7b36d91a4f4914e2267eef
haksuman <51818946+haksuman@users.noreply.github.com>
2022-06-06 23:01:15 +0300

    bug fixes and article scraping function update
    defining scraping function for article links and fixing bugs in that function

--- src/data/unv_scraping.py
+++ src/data/unv_scraping.py
@@ -3,32 +3,44 @@ from bs4 import BeautifulSoup
 
 print("working")
 
-page = requests.get(
-    "https://www.gm.undp.org/content/gambia/en/home/blog/2020/undp-support-to-the-establishment-of-the-virtual-courts-in-respo.html")
-soup = BeautifulSoup(page.content, 'html.parser')
+# list_path_article = []  # stands for relative path of document
+# list_prps_article = []  # stands for list of paragraphs in document
+# list_label_article = []  # stands for list of labels in document
 
-post_date = soup.find('p', class_='subheader').string  # document post date of page
-doc_title = soup.h1.get_text()  # document title in page
 
-label_content = soup.find('div', class_='pageSDGicons section')
-list_label_content = []  # list for SDG labels
-for item in label_content.findAll("img"):
-    list_label_content.append(item.get('alt'))
+def get_article_data(article_link):
+    page = requests.get(article_link)  # requesting and parsing with bs4
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-doc_paragraphs_parent = soup.find('div', class_='parbase section text')
-list_doc_paragraphs = []  # list for paragraphs in document
-for item in doc_paragraphs_parent.findAll("p"):
-    list_doc_paragraphs.append(item.string)
+    post_date = soup.find('p', class_='subheader').string  # document post date of page
+    doc_title = soup.h1.get_text()  # document title in page
 
-keywords_doc_parent = soup.find("meta", {"name": "keywords"})  # key word text
-keywords_doc = keywords_doc_parent['content']
+    label_content = soup.find('div', class_='pageSDGicons section')
+    list_label_content = []  # list for SDG labels
+    for item in label_content.findAll("img"):
+        list_label_content.append(item.get('alt'))
 
-# list for title page path
-path_doc = soup.find("ul", {'class': "breadcrumbs"})
-list_path_doc = []
-for item in path_doc.findAll('span', {'property': 'name'}):
-    list_path_doc.append(item.string)
+    doc_paragraphs_parent_set = soup.find_all('div', class_='parbase section text')
+    list_doc_paragraphs = []  # list for paragraphs in document / each paragraph is one list element
+    for doc_paragraphs_parent in doc_paragraphs_parent_set:
+        for item in doc_paragraphs_parent.findAll("p"):
+            list_doc_paragraphs.append(item.get_text())
 
-print(list_doc_paragraphs)
-print("completed")
+    keywords_doc_parent = soup.find("meta", {"name": "keywords"})  # key word text
+    keywords_doc = keywords_doc_parent['content']  # stored as str with "," between keywords
 
+    # list for title page path
+    path_doc = soup.find("ul", {'class': "breadcrumbs"})
+    list_path_doc = []
+    for item in path_doc.findAll('span', {'property': 'name'}):
+        list_path_doc.append(item.string)
+
+    return list_path_doc, post_date, doc_title, list_doc_paragraphs, list_label_content, keywords_doc
+
+
+list_path_article, post_date, doc_title, list_prps_article, list_label_article, keywords_doc = get_article_data(
+    "https://www.gw.undp.org/content/guinea_bissau/en/home/blog/design-thinking-as-a-powerful-tool-to-improve-public-service-del.html")
+
+print(list_path_article)
+
+#"https://www.gm.undp.org/content/gambia/en/home/blog/2020/undp-support-to-the-establishment-of-the-virtual-courts-in-respo.html" working link
