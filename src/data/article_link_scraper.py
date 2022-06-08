01a19948cf3f87f874418f0d4059d85f98f098da
haksuman <51818946+haksuman@users.noreply.github.com>
2022-06-09 00:16:17 +0300

    unavailable link elimination update

--- src/data/article_link_scraper.py
+++ src/data/article_link_scraper.py
@@ -4,10 +4,16 @@ import pandas as pd
 from project_dir_helper import project_dir
 import urllib.parse
 
-
 df_link_COs = pd.read_excel(project_dir + "/data/interim/output_links.xlsx", index_col=0)  # getting all generated links
-df_link_COs = df_link_COs.loc[df_link_COs['status'] == 200].reset_index()  # keeping only working links
+df_link_COs = df_link_COs.loc[df_link_COs['status'] == "200"].reset_index()  # keeping only working links
 
+def get_url_status(url):  # checks status for each url in list urls
+    try:
+        r = requests.get(url)
+        temp_status = str(r.status_code)
+    except Exception as e:
+        temp_status = "Failed to connect"
+    return temp_status
 
 def get_h4_tag(link_page):
     h4_link_list = []
@@ -21,7 +27,7 @@ def get_h4_tag(link_page):
         h4_text = h4.get_text(strip=True)  # getting h4 tag text
         h4_text_list.append(h4_text)  # appending obtained h4 text to list
         parent_link_list.append(link_page)
-        count_print = count_print+1
+        count_print = count_print + 1
         print(count_print)
     return parent_link_list, h4_link_list, h4_text_list  # retuning list of links and text as 2 lists
 
@@ -40,10 +46,23 @@ for link_article_parent_page in df_link_COs["link"]:
 df_link_articles = pd.DataFrame(
     {'parent_link': list_all_parent_link, 'link_article': list_all_h4_link, 'article_title': list_all_h4_text})
 
-for index_article, row_article in df_link_articles.iterrows(): # this for loop is stands for solving a bug that generate blog link as article link
-    if row_article["parent_link"]==row_article["link_article"]:
+for index_article, row_article in df_link_articles.iterrows():  # this for loop is stands for solving a bug that generate blog link as article link
+    if row_article["parent_link"] == row_article["link_article"]:
         df_link_articles.drop(index_article, axis=0, inplace=True)
 
-df_link_articles.to_excel(project_dir+"/data/interim/output_article_links.xlsx")
+step_counter2 = 0
+list_article_link_status = []
+for index_article_1, row_article_1 in df_link_articles.iterrows():
+    status_check = get_url_status(row_article_1["link_article"])
+    list_article_link_status.append(status_check)
+    step_counter2 = step_counter2 + 1
+    print(step_counter2)
+
+df_link_articles["article_link_status"] = list_article_link_status
+df_link_articles = df_link_articles.loc[df_link_articles['article_link_status'] == "200"].reset_index()  # keeping only working links
+
+
+
+df_link_articles.to_excel(project_dir + "/data/interim/output_article_links.xlsx")
 
 print("OK")
--- src/data/main_article_scraper.py
+++ src/data/main_article_scraper.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from unv_scraping import get_article_data
 from project_dir_helper import project_dir
+import requests
 
 df_article_scrape = pd.read_excel(project_dir + "/data/interim/output_article_links.xlsx",
                                   index_col=0)  # getting all generated article links
 
-df_article_scrape = df_article_scrape.head(500)  # in order to reduce number of rows in df
+# df_article_scrape = df_article_scrape.head(500)  # in order to reduce number of rows in df
 
 list_main_path_article = []
 list_main_post_date = []
@@ -14,7 +15,8 @@ list_main_prps_article = []
 list_main_label_article = []
 list_main_keywords_article = []
 
-count=1
+count = 1
+
 for index_article, row_article in df_article_scrape.iterrows():
     list_path_article, post_date, doc_title, str_prps_article, list_label_article, keywords_article = get_article_data(
         row_article["link_article"])
@@ -26,7 +28,7 @@ for index_article, row_article in df_article_scrape.iterrows():
     list_main_label_article.append(list_label_article)
     list_main_keywords_article.append(keywords_article)
     print(count)
-    count = count+1
+    count = count + 1
 
 df_article_scrape["path_article"] = list_main_path_article
 df_article_scrape["post_date"] = list_main_post_date
@@ -35,5 +37,6 @@ df_article_scrape["article_text"] = list_main_prps_article
 df_article_scrape["article_label"] = list_main_label_article
 df_article_scrape["article_keywords"] = list_main_keywords_article
 
-df_article_scrape.to_excel(project_dir+"/data/processed/article_scrape_data.xlsx")
+df_article_scrape.to_csv(project_dir + "/data/processed/article_scrape_data.csv")
 print("Completed!")
+a = 5
