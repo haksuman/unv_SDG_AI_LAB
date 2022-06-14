45e612bb3a4a01db6ca53772a6cc47781d1aa682
haksuman <51818946+haksuman@users.noreply.github.com>
2022-06-14 22:05:30 +0300

    Update for learn new output

--- src/data/main_article_scraper.py
+++ src/data/main_article_scraper.py
@@ -7,7 +7,7 @@ from src.features.project_functions import get_url_status
 df_article_scrape = pd.read_excel(project_dir + "/data/interim/output_article_links.xlsx",
                                   index_col=0)  # getting all generated article links
 
-# df_article_scrape = df_article_scrape.head(500)  # in order to reduce number of rows in df
+# df_article_scrape = df_article_scrape.head(10)  # in order to reduce number of rows in df
 
 list_main_path_article = []
 list_main_post_date = []
@@ -18,15 +18,20 @@ list_main_keywords_article = []
 
 
 step_counter = 0
-for index_status_check, row_status_check in df_article_scrape.iterrows():
-    status_check = get_url_status(row_status_check["link_article"])
-    if not str(status_check) == 200:
-        df_article_scrape.drop(index_status_check,inplace=True)
-    step_counter = step_counter + 1
-    print(step_counter)
+# for index_status_check, row_status_check in df_article_scrape.iterrows():
+#     status_check = get_url_status(row_status_check["link_article"])
+#     if not str(status_check) == 200:
+#         df_article_scrape.drop(index_status_check,inplace=True)
+#     step_counter = step_counter + 1
+#     print(step_counter)
 
 count = 1
 for index_article, row_article in df_article_scrape.iterrows():
+    status_check = get_url_status(row_article["link_article"])
+    if not str(status_check) == "200":
+        df_article_scrape.drop(index_article, inplace=True)
+        continue
+
     list_path_article, post_date, doc_title, str_prps_article, list_label_article, keywords_article = get_article_data(
         row_article["link_article"])
     # storing web scraping outputs in main_lists
